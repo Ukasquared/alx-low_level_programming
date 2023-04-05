df2fc05f055497585fa537bc500fb54ec2a44740
Ukasquared <ukachukwujudith95@gmail.com>
2023-04-04 21:16:15 -0700

    add a new node to linked list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+* add_nodeint - function that adds a new node
+* at the beginning of a listint_t list.
+* @head: linked list
+* @n: linked list data
+* Return: address of new element
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t ptr;
+
+	if (head == NULL)
+		return (NULL);
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->n = n;
+	ptr->next = *ptr;
+	*head = ptr;
+	return (ptr);
+
+}
