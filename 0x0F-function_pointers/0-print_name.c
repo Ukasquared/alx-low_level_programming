f1ac20c10bcfc6174be6b982c1b196ba8d9e336e
Ukasquared <ukachukwujudith95@gmail.com>
2023-04-17 22:32:50 -0700

    print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+* print name - prints a name
+* @name: name to print
+* @f: function pointer
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+		if(f != NULL)
+			f(name);
+}
