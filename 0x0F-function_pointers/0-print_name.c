7d74ebf62f947956c80d89cf3bbfc59777e0642b
Ukasquared <ukachukwujudith95@gmail.com>
2023-04-17 22:55:07 -0700

    writes a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,14 @@
 #include "function_pointers.h"
 /**
-* print name - prints a name
+* print_name - prints a name
 * @name: name to print
 * @f: function pointer
+* Return: void
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL)
-		if(f != NULL)
+		if (f != NULL)
 			f(name);
 }
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+* array_iterator - iterates the array
+* @array: array
+* @size: size if array
+* @action: function pointer
+* Return: void
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array != NULL && f != NULL && size != 0)
+	{
+		for (i = 0; i < size; i++)
+			(*f)(array[i];
+	}
+}
