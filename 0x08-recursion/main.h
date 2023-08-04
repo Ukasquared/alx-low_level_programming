65fe4fa406a570f24f832b836e990958dc8ad005
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-05 00:27:51 +0100

    header file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 #include <stdio.h>
+#include <string.h>
 int _putchar(char);
 void _puts_recursion(char *);
 void _print_rev_recursion(char *);
@@ -8,5 +9,6 @@ int _strlen_recursion(char *);
 int factorial(int);
 int _pow_recursion(int, int);
 int _sqrt_recursion(int);
-
+int is_prime_number(int);
+int is_palindrome(char *s);
 #endif
