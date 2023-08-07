b1b3d8b6b86f3490f818b79d200adfdbfd96b3f7
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-07 16:09:40 +0100

    program that print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - prints name followed by new line
+*
+* @argc: argument count
+* @argv: argument vector
+*  Return: 0 or 1
+*/
+
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
