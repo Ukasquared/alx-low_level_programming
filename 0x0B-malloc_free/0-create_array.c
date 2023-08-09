a0c73ac844c834427a2d4695a82dcb2200530f1c
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-09 05:02:19 +0100

    allocated memory for array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* create_array - creates an array of chars,
+* and initialize it with a specific char
+*
+* @size: size of array
+* @c: specific character
+*  Return: pointer to the character
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	ptr = malloc((sizeof(char) * size) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size + 1; i++)
+		*(ptr + i) = c;
+	*(ptr + i) = '\0';
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef main_h
+#define main_h
+
+#include <stdlib.h>
+char *create_array(unsigned int, char);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+
+#endif
