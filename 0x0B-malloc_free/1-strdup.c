04d588c210a9ad71e051150a60452433fd527b26
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-09 05:46:18 +0100

    return pointer to a copied string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+* _strdup - returns a pointer to a newly allocated
+* space in memory, which contains a copy of
+* the string given as a parameter.
+*
+* @str: string to be copied
+*  Return: pointer
+*/
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (*str == '\0')
+		return (NULL);
+	ptr = malloc(sizeof(char) * _strlen(str) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		ptr[i] = str[i];
+	return (ptr);
+}
+
+
+/**
+* _strlen - find length of string
+*
+* @str: string
+* Return: the length
+*/
+
+int _strlen(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
