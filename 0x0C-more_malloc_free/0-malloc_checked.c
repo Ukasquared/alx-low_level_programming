ae13dd35a51e9d558329e93b8e4accf244bb57bd
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-11 05:56:10 +0100

    memory allocation

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* malloc_checked - prints number of argument passed
+*
+* @b: value to store in heap
+*  Return: pointer to allocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(int));
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef main_h
+#define main_h
+
+#include <stdlib.h>
+void *malloc_checked(unsigned int);
+
+#endif
