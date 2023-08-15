a83edaa5c152bd4e836a794418013e9680ec2cf5
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-15 05:39:49 +0100

    structures in c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+* init_dog - initializse a vairiabe of type struct dog
+*
+* @d: pointer to struct
+* @name: name of dog
+* @age: dog's age
+* @owner: owner's name
+* Return: void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef dog_h
+#define dog_h
+
+#include <stdio.h>
+
+/**
+* struct dog - a new structure template
+* @name: name of dog
+* @age: age of dog
+* @owner: name of owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+void init_dog(struct dog *, char *, float, char *);
+
+#endif
