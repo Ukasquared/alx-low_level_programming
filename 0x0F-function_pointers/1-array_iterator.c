0c75cf9d8df5038936beabdd9680c651e14d7961
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-17 05:07:42 +0100

    executes a funcion given as a parameter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - function that executes a function
+* given as a parameter on each element of array
+* @array: pointer to the array
+* @size: size of array
+* @action: pointer to the function that will be used
+*
+* Return: nothing
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		(*action)(*(array + i));
+}
