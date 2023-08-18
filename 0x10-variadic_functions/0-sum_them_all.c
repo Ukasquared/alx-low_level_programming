174287fee7f6937ac19998ed0338e0c7f19ff6f2
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-18 05:52:29 +0100

    sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all -- sum of all its parameter
+ * @n: numbers
+ * Return:  0.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	
+	if (n == 0)
+		return (0);
+	
