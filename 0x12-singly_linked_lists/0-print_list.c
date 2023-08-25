c74c5facf64e66c6c449424fa47b3170a6c1ce64
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-25 02:46:00 +0100

    prints node

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,8 +16,8 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%u ]", !h->str ? 0 : h->len);
-		printf("%s\n", !h->str ? "(nil)" : h->str);
+		printf("[%u]", !h->str ? 0 : h->len);
+		printf(" %s\n", !h->str ? "(nil)" : h->str);
 		h = h->next;
 		count++;
 	}
