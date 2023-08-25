bd142c474e5bb859a2803632569310b37984fbf3
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-25 02:16:13 +0100

    prints node

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - check the code
+ *
+ * @h: takes the list
+ * Return: Always 0.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+	
+	while (h != NULL)
+	{
+		printf("[%u]", !h->str ? 0 : h->len);
+		printf("%s\n", !h->str ? "(nil)" : h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
