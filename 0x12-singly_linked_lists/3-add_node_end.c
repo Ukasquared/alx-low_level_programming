3c230ac5b9f39f4763271636ce9db8e6632082ad
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-26 04:21:49 +0100

    add node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,14 +12,17 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	list_t *newNode = malloc(sizeof(list_t));
 	list_t *mov_ptr = *head;
+	size_t len;
 
 	if (newNode == NULL)
 		return (NULL);
+	for (len = 0; str[len] != '\0'; len++)
+		;
 	newNode->str = strdup(str);
-	newNode->len = strlen(str);
-	if (*head == NULL)
+	newNode->len = len;
+	if (mov_ptr == NULL)
 		*head = newNode;
-	else if (*head != NULL)
+	else if (mov_ptr != NULL)
 	{
 		while (mov_ptr->next != NULL)
 			mov_ptr = mov_ptr->next;
