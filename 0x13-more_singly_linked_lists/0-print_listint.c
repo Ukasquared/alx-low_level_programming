39d25c94d414527f41b52777cf5250294650eadd
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-28 21:13:55 +0100

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - check the code
+ * print_listint - print linked list
  *
  * @h:linked list
  * Return: number of nodes.
@@ -9,15 +9,16 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int count;
-	listint_t mov_ptr;
+	int count = 0;
+	const listint_t *mov_ptr;
+	mov_ptr = h;
 
 	if (h == NULL)
-		return (NULL);
-	while (h != NULL)
+		return (0);
+	while (mov_ptr != NULL)
 	{
 		count++;
-		print("%d", mov_ptr->n);
+		printf("%d\n", mov_ptr->n);
 		mov_ptr = mov_ptr->next;
 	}
 	return (count);
