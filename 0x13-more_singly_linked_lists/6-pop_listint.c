ae8f961aa3d0bf85d742b81ee3eac05730edafc4
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-29 06:37:58 +0100

    pop list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,9 +1,27 @@
 #include "lists.h"
 
 /**
- * print_listint - check the code
- * 
- * @h:linked list
- * Return: number of nodes.
+ * pop_listint - check the code
+ *
+ * @head:linked list
+ * Return: o if linked list is empty
  */
 
+int pop_listint(listint_t **head)
+{
+	listint_t *mov_ptr;
+	int node_data;
+
+	if (*head == NULL)
+		return (0);
+	if (*head != NULL)
+	{
+		mov_ptr = *head;
+		node_data = mov_ptr->n;
+		*head = (*head)->next;
+		free(mov_ptr);
+	}
+	if (!node_data)
+		return (0);
+	return (node_data);
+}
