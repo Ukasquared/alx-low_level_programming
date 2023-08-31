d7e9f17e84d1485ce5b0d18b4ca0bfd42b261cd6
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-31 03:54:20 +0100

    insert a node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,17 +19,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = n;
-	if (!(*head) && idx == 0)
-	{	*head = newNode;
-		return (*head);
-	}
-	if (*head && idx == 0)
+	if (idx == 0)
 	{
 		newNode->next = *head;
 		*head = newNode;
 		return (*head);
 	}
-	while (mov_ptr->next != NULL)
+	while (count <= idx)
 	{
 		if (count == idx)
 		{
