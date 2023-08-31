df8065868aec855fc15ca6b0b2b257bde6657c7c
Ukasquared <ukachukwujudith95@gmail.com>
2023-08-31 03:38:47 +0100

    insert a node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,16 +29,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = newNode;
 		return (*head);
 	}
-	while (mov_ptr->next != NULL && count <= idx)
+	while (mov_ptr->next != NULL)
 	{
-			if (count == idx)
-			{
-				newNode->next = mov_ptr->next;
-				mov_ptr->next = newNode;
-				return(*head);
-			}
-			mov_ptr = mov_ptr->next;
-			count++;
+		if (count == idx)
+		{
+			newNode->next = mov_ptr->next;
+			mov_ptr->next = newNode;
+			return (*head);
+		}
+		mov_ptr = mov_ptr->next;
+		count++;
 	}
 	return (NULL);
 }
