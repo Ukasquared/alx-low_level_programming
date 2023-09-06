6df9967dd1185a20ed06888bafbb552a55fba6a9
Ukasquared <ukachukwujudith95@gmail.com>
2023-09-06 02:06:16 +0100

    pprint to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,15 +15,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t c, c1;
 	char *buf;
 
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
 	if (!filename)
 		return (0);
 	fd_no = open(filename, O_RDONLY);
+	if (fd_no == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		close(fd_no);
+		return (0);
+	}
 	c = read(fd_no, buf, letters);
-	c1 = write(STDOUT_FILENO, buf, letters);
-	if (c1 != c || c1 == -1 || c == -1 || fd_no == -1)
+	c1 = write(STDOUT_FILENO, buf, c);
+	if (c1 != c || c1 == -1 || c == -1)
 	{
 		free(buf);
 		return (0);
