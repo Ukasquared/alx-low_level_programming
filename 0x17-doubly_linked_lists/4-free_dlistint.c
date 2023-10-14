f8e169387eb65928ad46d113a8804237aaf2b471
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-14 15:00:10 +0100

    corrected

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -10,11 +10,9 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *ptr = head;
 
-	while (ptr->next)
+	while (ptr)
 	{
 		ptr = ptr->next;
-		free(ptr->prev);
+		free(ptr);
 	}
-	free(ptr);
-	ptr = NULL;
 }
