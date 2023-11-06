36c9713de67baf36b9e0d424c5cfc3fcb6f36dd4
Ukasquared <ukachukwujudith95@gmail.com>
2023-11-06 17:23:16 +0100

    gts the index value

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - the index of key
+ * @key: key
+ * @size: size of the array of the hash table
+ *
+ * Return: the index at which the key/value pair
+ * should be stored in the array of the hash table
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_value = hash_djb2(key);
+	return (hash_value % size);
+}
