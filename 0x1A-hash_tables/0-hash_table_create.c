80fe3b29a2a1ec57c06d8728b571eddc884fbe01
Ukasquared <ukachukwujudith95@gmail.com>
2023-11-09 05:52:42 +0100

    create  hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,14 +2,17 @@
 
 /**
  * hash_table_create - creates a hash table
+ *
  * @size: the size of the array
  * Return: pointer to the newly created hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = NULL;
+	hash_table_t *table;
 	unsigned long int i;
+
+	table = NULL;
 
 	/* allocate memory for struct) */
 	table = malloc(sizeof(table));
@@ -20,6 +23,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	table->size = size;
 	for (i = 0; i < size; i++)
+	{
 		*((table->array) + i) = NULL;
+	}
 	return (table);
 }
