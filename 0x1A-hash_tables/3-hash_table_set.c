be109a4d92880eaf48a09835f16bceecf89da485
Ukasquared <ukachukwujudith95@gmail.com>
2023-11-09 05:18:19 +0100

    adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,39 +35,46 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 int create_pair(hash_table_t *ht, const unsigned char *key, const char *value, unsigned long int hash_value)
 {
-	hash_node_t **arr, *current, *new_node;
+	hash_node_t *temp, *new_node;
 	const char *keys = (const char*)key;
-
-	current = NULL;
-	arr = NULL;
+	char *new_value;
 
 	/* test case */
-	if (!ht)
+	if (!ht || !keys || *keys == '\0' || !value)
 		return (0);
-	arr = ht->array;
 
-	/*get the array at hash_value */
-	current = arr[hash_value];
 	/* if no collision */
-	if (current == NULL)
+	if (ht->array[hash_value] == NULL)
 	{
 		/* allocate memory */
-		current = malloc(sizeof(hash_node_t));
-		if (current == NULL)
-			return (0);
-		current->key = strdup(keys);
-		current->value = strdup(value);
-		if (!current->key || !current->value)
-			return (0);
-	}
-	else
-	{
-		/* in the case of collision */
-		/* add node at the begining of list */
-		if (current != NULL)
-			new_node = add_node(&current, keys, value);
+		new_node = add_node(keys, value);
 		if (new_node == NULL)
 			return (0);
+		ht->array[hash_value] = new_node;
+	}
+
+	if (ht->array[hash_value])
+	{
+		/* in the case of collision */
+		/* update value */
+		if (strcmp(ht->array[hash_value]->key, keys) == 0)
+		{
+			free(ht->array[hash_value]->value);
+			new_value = strdup(value);
+			if (!new_value)
+				return(0);
+			ht->array[hash_value]->value = new_value;
+		}
+		else
+		{
+			/* add node at the begining of list */
+			temp = ht->array[hash_value];
+			new_node = add_node(keys, value);
+			if (new_node == NULL)
+				return (0);
+			ht->array[hash_value] = new_node;
+			new_node->next = temp;
+		}
 	}
 	return (1);
 }
@@ -81,22 +88,17 @@ int create_pair(hash_table_t *ht, const unsigned char *key, const char *value, u
 * Return: new node if successful or NULL
 */
 
-hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+hash_node_t *add_node(const char *key, const char *value)
 {
-	hash_node_t *temp;
+	hash_node_t *node;
 
-	if (*head == NULL)
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 		return (NULL);
-	if (!key || !value)
+	node->key = strdup(key);
+	node->value = strdup(value);
+	/* test case for stdrup */
+	if (!(node)->key || !(node)->value)
 		return (NULL);
-	temp = *head;
-	*head = malloc(sizeof(hash_node_t));
-	if (*head == NULL)
-		return (NULL);
-	(*head)->key = strdup(key);
-	(*head)->value = strdup(value);
-	if (!(*head)->key || !(*head)->value)
-		return (NULL);
-	(*head)->next = temp;
-	return (*head);
+	return (node);
 }
--- 0x1A-hash_tables/add_node.c
+++ 0x1A-hash_tables/add_node.c
@@ -8,22 +8,15 @@
 * Return: new node if successful or NULL
 */
 
-hash_node_t *add_node(hash_node_t **head, char *key, char *value)
+hash_node_t *add_node(char *key, char *value)
 {
-	hash_node_t *temp;
-
-	if (*head == NULL)
+	hash_node_t *node
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 		return (NULL);
-	if (!key || !value)
+	node->key = strdup(key);
+	node->value = strdup(value);
+	if (!(node->key) || !(node->value))
 		return (NULL);
-	temp = *head;
-	*head = malloc(sizeof(hash_node_t));
-	if (*head == NULL)
-		return (NULL);
-	(*head)->key = strdup(key);
-	(*head)->value = strdup(value);
-	if (!(*head)->key || !(*head)->value)
-		return (NULL);
-	(*head)->next = temp;
-	return (*head)
+	return (node);
 }
