9822402e282c6dcf998b11b9a50f02ad3802c279
Ukasquared <ukachukwujudith95@gmail.com>
2023-12-28 08:07:14 +0100

    compute perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,28 +7,22 @@ def island_perimeter(grid):
     calculates and returns the
     perimeter of an island
     """
-    if grid is None:
-        return
-    if grid == [] or not isinstance(grid, list):
-        return
-    peri = 0
-    row = len(grid)
-    col = len(grid[0])
-    if 1 in grid[0] or 1 in grid[row - 1]:
-        return
-    for i in range(row):
-        for j in range(col):
-            if grid[i][j] == 1:
-                peri += 4
-                if grid[i - 1][j] == 1:
-                    peri -= 1
-                if grid[i + 1][j] == 1:
-                    peri -= 1
-                if grid[i][j - 1] == 1:
-                    peri -= 1
-                if grid[i][j + 1] == 1:
-                    peri -= 1
-    if peri:
-        return peri
-    else:
-        return
+    if grid is not None:
+        if grid != [] or isinstance(grid, list):
+            peri = 0
+            row = len(grid)
+            col = len(grid[0])
+            if 1 not in grid[0] and 1 not in grid[row - 1]:
+                for i in range(row):
+                    for j in range(col):
+                        if grid[i][j] == 1:
+                            peri += 4
+                            if grid[i - 1][j] == 1:
+                                peri -= 1
+                            if grid[i + 1][j] == 1:
+                                peri -= 1
+                            if grid[i][j - 1] == 1:
+                                peri -= 1
+                            if grid[i][j + 1] == 1:
+                                peri -= 1
+                return peri
