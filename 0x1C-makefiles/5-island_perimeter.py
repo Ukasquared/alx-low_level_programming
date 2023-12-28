06b983f6a71004069b97f4ea1124840d48ec44a5
Ukasquared <ukachukwujudith95@gmail.com>
2023-12-28 10:12:28 +0100

    compute the perimeter of an island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,13 +13,13 @@ def island_perimeter(grid):
     for i in range(row):
         for j in range(col):
             if grid[i][j] == 1:
-                 peri += 4
-                 if i > 0 and grid[i - 1][j] == 1:
-                     peri -= 1
-                 if i < (row - 1) and grid[i + 1][j] == 1:
-                     peri -= 1
-                 if j > 0 and grid[i][j - 1] == 1:
-                     peri -= 1
-                 if j < (col - 1)  and  grid[i][j + 1] == 1:
-                     peri -= 1
+                peri += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    peri -= 1
+                if i < (row - 1) and grid[i + 1][j] == 1:
+                    peri -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    peri -= 1
+                if j < (col - 1) and grid[i][j + 1] == 1:
+                    peri -= 1
     return peri
