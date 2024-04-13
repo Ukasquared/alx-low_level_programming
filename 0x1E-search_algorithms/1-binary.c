8eb717bd0d38e94624aab3b5d214ff0891d9afd5
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-04-13 13:02:07 +0100

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -5,7 +5,7 @@
  * @min_index: the minimum index of array
  * @max_index: the maximum index of array
  * @value: the value to be searched for
- * @array: the array to search for value
+ * @arr: the array to search for value
  * Return: the found index or -1
  */
 
@@ -13,25 +13,24 @@ int find_val(int *arr, size_t min_index, size_t max_index, int value)
 {
 	size_t i, mid;
 
-	if ( min_index <= max_index) {	
+	if ( min_index <= max_index) {
 		printf("Searching in array: ");
 		for (i = min_index; i <= max_index; i++) {
 			if (i < max_index)
 				printf("%d, ", arr[i]);
 			else
-				printf("%d", arr[i]);		
+				printf("%d", arr[i]);
 		}
 		printf("\n");
 		mid = (min_index + max_index) / 2;
-		if (arr[mid] == value) {
+		if (arr[mid] == value)
 			return (mid);
-		}
 		if (arr[mid] < value) {
 			min_index = mid + 1;
 			return (find_val(arr, min_index, max_index, value));
 		}
 		if (arr[mid] > value) {
- 			max_index = mid - 1;
+			max_index = mid - 1;
 			return (find_val(arr, min_index, max_index, value));
 		}
 	}
@@ -51,6 +50,7 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t min, max;
 	int index;
+
 	if (!array || size == 0)
 		return (-1);
 	min = 0, max = size - 1;
